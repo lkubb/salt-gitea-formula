6fd0bbdf3d2ab262962828871aa8fabe5df9e9df
jeanluc <lkubb@protonmail.com>
2024-02-17 19:11:28 +0100

    fix: Update template

--- test/integration/default/controls/config.rb
+++ test/integration/default/controls/config.rb
@@ -24,10 +24,10 @@ control 'gitea.config.file' do
       # rubocop:disable Lint/RedundantCopDisableDirective
       # rubocop:disable Layout/LineLength
       should include(
-        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", '\
-        '"roles", "osfinger", "os", "os_family"], "source_files": '\
-        '{"gitea-config-file-file-managed": ["example.tmpl.jinja"], '\
-        '"gitea--config-file-file-managed": '\
+        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", ' \
+        '"roles", "osfinger", "os", "os_family"], "source_files": ' \
+        '{"gitea-config-file-file-managed": ["example.tmpl.jinja"], ' \
+        '"gitea--config-file-file-managed": ' \
         '["-example.tmpl.jinja"]}'
       )
       # rubocop:enable Layout/LineLength
